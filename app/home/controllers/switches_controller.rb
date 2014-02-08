982d6a2351d0a5f11334cf0aaa17059a7eee9a8d
Ryan Stout <ryanstout@gmail.com>
2014-02-07 21:47:25 -0700

    Switches

--- app/home/controllers/switches_controller.rb
+++ app/home/controllers/switches_controller.rb
@@ -4,11 +4,11 @@ class SwitchesController < ModelController
   def initialize
     super
     
-    self._c1 = self._box1.or(self._box4)
-    self._c2 = self._box2.or(self._box4).or(self._box5)
-    self._c3 = self._box3.or(self._box5)
-    # self._middle = self._box3.and((self._box1).or(self._box2))
-    # self._light1 = self._box1.or(self._middle)
-    # self._light2 = self._box2.or(self._middle)
+    self._p1 = _box4.and(_box1.or(_box2))
+    self._p2 = _box5.and(_box2.or(_box3))
+    
+    self._c1 = _box1.or(_p1).or(_p2.and(_box4))
+    self._c2 = _box2.or(_p1).or(_p2)
+    self._c3 = _box3.or(_p2).or(_p1.and(_box5))
   end
 end
